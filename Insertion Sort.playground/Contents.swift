9c6b1c4c0ab8b470e1abbc5092442fb53355eccc
LiulietLee <lee.liuliet@hotmail.com>
2016-02-21 15:28:45 +0800

    sorting

---
+++ Insertion Sort.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func sorting(var array: [Int]) -> [Int] {
+    for var i = 1; i < array.count; i++ {
+        var key = array[i]
+        for var j = i - 1; j >= 0; j-- {
+            if array[j] > key {
+                array[j + 1] = array[j]
+                array[j] = key
+                key = array[j]
+            } else {
+                break
+            }
+        }
+    }
+    
+    return array
+}
+
+let array = [5, 4, 1, 2, 6, 3, 233]
+
+let sortedArray = sorting(array)
+
+print(sortedArray)
+
+/*
+
+result: 
+[1, 2, 3, 4, 5, 6, 233]
+
+*/
