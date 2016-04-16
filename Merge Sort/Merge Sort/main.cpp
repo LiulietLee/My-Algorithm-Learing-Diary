d454b0349f206f4225c9b28a9f39d1f03ba97815
LiulietLee <lee.liuliet@hotmail.com>
2016-04-16 20:59:18 +0800

    update

--- Merge Sort/Merge Sort/main.cpp
+++ Merge Sort/Merge Sort/main.cpp
@@ -10,32 +10,66 @@
 
 using namespace std;
 
-int * insertionSort(int preArray[], int length) {
-    for (int i = 1; i < length; i++) {
-        if (preArray[i] < preArray[i - 1]) {
-            for (int j = i; j > 0; j--) {
-                if (preArray[j] < preArray[j - 1]) {
-                    int temp = preArray[j];
-                    preArray[j] = preArray[j - 1];
-                    preArray[j - 1] = temp;
-                } else {
-                    break;
-                }
-            }
+void merge(int *a, int low, int high, int mid)
+{
+    int i, j, k, c[50];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            c[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            c[k] = a[j];
+            k++;
+            j++;
         }
     }
-    
-    return preArray;
+    while (i <= mid)
+    {
+        c[k] = a[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        c[k] = a[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)
+    {
+        a[i] = c[i];
+    }
 }
 
-int main(int argc, const char * argv[]) {
-    int preArray[] = {2, 1, 4, 3, 6, 5, 2333, 233, 23};
-    int length = sizeof(preArray) / sizeof(preArray[0]);
-    int *sortedArray = insertionSort(preArray, length);
-    
-    for (int i = 0; i < length; i++) {
-        cout << sortedArray[i] << endl;
+void mergesort(int *a, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,high,mid);
     }
+    return;
+}
 
-    return 0;
+int main()
+{
+    int a[5] = {5, 4, 3, 2, 1};
+
+    mergesort(a, 0, 4);
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<a[i];
+    }
 }
