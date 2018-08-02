68566082077bedd6400ba3733dbceb5e26bbbf92
LiulietLee <lee.liuliet@hotmail.com>
2018-08-02 22:49:24 +0800

    % -> &

--- 数学相关/快速幂取模.cpp
+++ 数学相关/快速幂取模.cpp
@@ -7,7 +7,7 @@ ll powerMod(ll a, ll b, ll c) {
     ll ans = 1;
     a %= c;
     while (b > 0) {
-        if (b % 2 == 1) ans = (ans * a) % c;
+        if (b & 1) ans = (ans * a) % c;
         b /= 2;
         a = (a * a) % c;
     }
