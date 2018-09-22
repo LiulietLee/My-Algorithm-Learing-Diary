7233d19a20609447f3761c0be0a1706e045a3ff8
LiulietLee <lee.liuliet@hotmail.com>
2018-09-22 22:20:36 +0800

    double gcd

--- 数学相关/辗转相除求最大公因数（GCD）.cpp
+++ 数学相关/辗转相除求最大公因数（GCD）.cpp
@@ -2,3 +2,9 @@ int gcd(int a, int b) {
 	if (b == 0) { return a; }
 	return gcd(b, a % b);
 }
+
+double gcd(double a, double b) {
+	if (fabs(a) < EPS) return b;
+	if (fabs(b) < EPS) return a;
+	return gcd(b, fmod(a, b));
+}
