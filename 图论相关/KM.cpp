fa9c27e562de286c57e2b5ab9b47ec78c8e3cfbd
LiulietLee <lee.liuliet@hotmail.com>
2018-10-11 12:22:27 +0800

    update km

--- 图论相关/KM.cpp
+++ 图论相关/KM.cpp
@@ -1,94 +1,100 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <cmath>
-#include <map>
-#include <vector>
-#include <queue>
-#include <ctime>
-#include <cstring>
-#include <algorithm>
-#define MAXSIZE 10010
-#define MAXN 20
-#define MAXND MAXN / 10
-#define MAXVERTEX 100010
-#define MAXEDGE 3000010
-#define INF 99999999
-#define nil NULL
-typedef long long ll;
-using namespace std;
-
-int n;
-int w[MAXN][MAXN];
-int lx[MAXN],ly[MAXN];
-int matched[MAXN];
-int slack[MAXN];
-bool s[MAXN],t[MAXN];
-
-bool match(int i){
-    s[i]=1;
-    for(int j=1;j<=n;j++){
-        int cnt=lx[i]+ly[j]-w[i][j];
-        if(cnt==0&&!t[j]){
-            t[j]=1;
-            if(!matched[j]||match(matched[j])){
-                matched[j]=i;
-                return 1;
-            }
-        }
-        else{
-            slack[j]=min(slack[j],cnt);
-        }
-    }
-    return 0;
-}
-
-void update(){
-    int a=INF;
-    for(int i=1;i<=n;i++){
-        if(!t[i])a=min(a,slack[i]);
-    }
-    for(int i=1;i<=n;i++){
-        if(s[i])lx[i]-=a;
-        if(t[i])ly[i]+=a;
-    }
-}
-
-void km(){
-    memset(matched,0,sizeof(matched));
-    memset(lx,0,sizeof(lx));
-    memset(ly,0,sizeof(ly));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            lx[i]=max(lx[i],w[i][j]);
-        }
-    }
-    for(int i=1;i<=n;i++){
-        memset(slack,0x3f,sizeof(slack));
-        while(1){
-            memset(s,0,sizeof(s));
-            memset(t,0,sizeof(t));
-            if(match(i))break;
-            else update();
-        }
-    }
-}
-
-int main()
+struct KM
 {
-    while(~scanf("%d",&n)){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                scanf("%d",&w[i][j]);
+    int w[maxn][maxn];
+    int n,m;
+    int lx[maxn],ly[maxn];
+    bool vx[maxn],vy[maxn];
+    int left[maxn];
+    int delta;
+    int slack[maxn];
+
+    bool match(int i,int flag)
+    {
+        vx[i] = true;
+        rep(j,1,m+1)if(!vy[j])
+        {
+            if(lx[i]+ly[j]==w[i][j])//相等子图
+            {
+                vy[j] = true;
+                if(left[j] == 0 || match(left[j],flag))
+                {
+                    if(flag) left[j] = i;
+                    return true;
+                }
             }
+            else slack[j] = min(slack[j],lx[i]+ly[j]-w[i][j]);
         }
-        km();
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            ans+=lx[i];
-            ans+=ly[i];
-        }
-        printf("%d\n",ans);
+        return false;
     }
-    return 0;
-}
+
+    int km(int n,int m)
+    {
+        this->n = n;
+        this->m = m;
+        memset(left,0,sizeof left);
+        ms(ly);
+        rep(i,1,n+1)
+        {
+            lx[i] = -inf;
+            
+            rep(j,1,m+1)
+                lx[i] = max(lx[i],w[i][j]);
+        }
+
+        rep(i,1,n+1)
+        {
+            rep(j,1,m+1)slack[j] = inf;
+            ms(vx),ms(vy);
+            if(match(i,1))continue;//增广路
+
+            bool ok = true;
+            while(ok)
+            {   
+                int  d = inf;
+                rep(j,1,m+1)if(!vy[j] && d>slack[j])
+                    d = slack[j];
+                
+                rep(j,1,n+1)if(vx[j])
+                    lx[j] -= d;
+
+                int p;
+                rep(j,1,m+1)    
+                if(vy[j])ly[j]+=d;
+                else 
+                {
+                    slack[j] -= d;
+                    if(slack[j] == 0)
+                        p = j;
+                }   
+
+                // rep(i,1,n+1)
+                // {
+                //  if(!vy[i] && slack[i] == 0)
+                //  {
+                //      vy[i] = true;
+                //      if(left[i] == 0 || match(left[i],0))
+                //      {
+                //          ok = false;
+                //          break;
+                //      }
+                //  }
+                // }
+             
+                vy[p] = true;
+                if(left[p] == 0 || match(left[p],0))
+                {
+                    ok = false;//招到合适的右端点，使得可以继续增广
+                }
+            
+            }
+            ms(vx),ms(vy);
+            match(i,1);     
+        }
+        int ans = 0;
+        rep(i,1,m+1)if(left[i])
+        {   
+       		ans+=w[left[i]][i];         
+        }
+        return ans;
+    }
+}km;
