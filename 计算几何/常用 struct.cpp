5d8d18a8011f29dda4616ad94087cf7ded13b399
LiulietLee <lee.liuliet@hotmail.com>
2018-10-21 19:50:27 +0800

    update 计算几何

--- 计算几何/常用 struct.cpp
+++ 计算几何/常用 struct.cpp
@@ -1,123 +1,113 @@
 struct Point {
     double x, y;
-    Point(double x = 0, double y = 0): x(x), y(y) {}
 };
 
-// 定义两点减法
-Point operator - (const Point &a, const Point &b) {
-    return Point(a.x - b.x, a.y - b.y);
+typedef Point Vector;
+
+Vector operator + (Vector a, Vector b) { return Vector{a.x + b.x, a.y + b.y}; }
+Vector operator - (Vector a, Vector b) { return Vector{a.x - b.x, a.y - b.y}; }
+Vector operator * (Vector a, double b) { return Vector{a.x * b, a.y * b}; }
+Vector operator / (Vector a, double b) { return Vector{a.x / b, a.y / b}; }
+
+bool operator < (const Point &a, const Point &b) {
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
 }
 
-Point operator + (const Point &a, const Point &b) {
-    return Point(a.x + b.x, a.y + b.y);
+const double eps = 1e-10;
+
+int dcmp(double x) {
+    if (fabs(x) < eps) return 0; return x < 0 ? -1 : 1;
 }
 
-struct Circle {
-    Point center;
-    double radius;
-    Circle(Point c = Point(), double r = 0): center(c), radius(r) {}
-};
-
-struct Vector {
-    double x, y;
-    Vector(double x = 0, double y = 0): x(x), y(y) {}
-    Vector(Point p): x(p.x), y(p.y) {}
-    // 求该向量的正交单位向量
-    Vector vertical() {
-        double dis = sqrt(x * x + y * y);
-        return Vector(-y / dis, x / dis);
-    }
-    // 取相反向量
-    Vector reverse() {
-        return Vector(-x, -y);
-    }
-    // 求向量的模
-    double length() {
-        return sqrt(x * x + y * y);
-    }
-    // 返回该向量延逆时针方向旋转至某一角度后的向量
-    Vector rotate(double angle) {
-        return Vector(x * cos(angle) - y * sin(angle), y * cos(angle) + x * sin(angle));
-    }
-    // 向量转化为点坐标
-    Point toPoint() {
-        return Point(x, y);
-    }
-};
-
-// 向量数量积
-double operator * (const Vector &a, const Vector &b) {
-    return a.x * b.x + a.y * b.y;
-}
-// 向量数乘
-Vector operator * (const double &a, const Vector &b) {
-    return Vector(b.x * a, b.y * a);
+bool operator == (const Point &a, const Point &b) {
+    return dcmp(a.x - b.x) == 0 && dcmp(a.y - b.y) == 0;
 }
 
-// 向量叉积的模
-double cross(Vector a, Vector b) {
-    return a.x * b.y - a.y * b.x;
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double length(Vector a) { return sqrt(dot(a, a)); }
+double angle(Vector a, Vector b) { return acos(dot(a, b) / length(a) / length(b)); }
+
+double cross(Vector a, Vector b) { return a.x * b.x - a.y * b.y; }
+
+/// 求三角形有向面积的二倍
+double area(Point a, Point b, Point c) { return cross(b - a, c - a); }
+
+Vector rotate(Vector a, double rad) {
+    return Vector{a.x * cos(rad) - a.y * sin(rad), a.x * sin(rad + a.y * cos(rad))};
 }
 
-// 求两向量夹角余弦值
-double cosValueOfTwoVectors(Vector u, Vector v) {
-    return (u * v) / (u.length() * v.length());
+/// 求单位法向量（左转 90°）
+Vector normal(Vector a) {
+    double l = length(a);
+    return Vector{-a.y / l, a.x / l};
 }
 
-bool doubleEqual(double a, double b) {
-    return fabs(a - b) < EPS;
-}
-
+/**
+ 直线的表示形式之一，具体为 p' = p + tv。
+ */
 struct Line {
-    Point p1, p2;
-    Line(Point p1 = Point(), Point p2 = Point()): p1(p1), p2(p2) {}
-    // 取线段中点
-    Point midPoint() {
-        return Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
-    }
-    // 线段转化为向量，方向为 p2 -> p1
-    Vector toVector() {
-        return Vector(p1 - p2);
-    }
-    // 获取线段长度
-    double length() {
-        return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-    }
-    // 判断点是否在线上
-    bool isContainPoint(Point p) {
-        return doubleEqual(cross(Vector(p1 - p), Vector(p2 - p)), 0) && Vector(p1 - p) * Vector(p2 - p) < EPS;
+    Point p;
+    Vector v;
+    
+    Point point(double t) {
+        return p + v * t;
     }
 };
 
-struct InfinityLine {
-    double k, b;
-    InfinityLine(double k = 0, double b = 0):
-        k(k), b(b) {}
-    // 获取 x 对应的 y 值
-    double valueOf(double x) {
-        return k * x + b;
-    }
-};
-
-// 求两直线交点，没有交点时返回 nil
-Point* intersectionOfInfinityLines(InfinityLine a, InfinityLine b) {
-    if (doubleEqual(a.k, b.k)) return nil;
-    static Point p = Point();
-    double x = (b.b - a.b) / (a.k - b.k);
-    double y = a.valueOf(x);
-    p = Point(x, y);
-    return &p;
+/// 求两直线交点（调用前需要确保两直线不重合）
+Point lineIntersection(Point p, Vector v, Point q, Vector w) {
+    Vector u = p - q;
+    double t = cross(w, u) / cross(v, w);
+    return p + v * t;
 }
 
-// 求两线段交点，没有交点时返回 nil
-Point* intersectionOfLines(Line a, Line b) {
-    if (doubleEqual(cross(a.toVector(), b.toVector()), 0)) return nil;
-    static Point p = Point();
-    Vector offset = (cross(b.toVector(), Vector(b.p1 - a.p1)) / cross(b.toVector(), Vector(a.p2 - a.p1))) * Vector(a.p2 - a.p1);
-    p = a.p1 + offset.toPoint();
-    if (a.isContainPoint(p) && b.isContainPoint(p)) {
-        return &p;
-    } else {
-        return nil;
-    }
+/// 点到直线距离，p 为点，ab 为直线
+double distanceToLine(Point p, Point a, Point b) {
+    Vector v1 = b - a, v2 = p - a;
+    return fabs(cross(v1, v2) / length(v1));
+}
+
+/// 点到线段距离
+double distanceToSegment(Point p, Point a, Point b) {
+    if (a == b) return length(p - a);
+    Vector v1 = b - a, v2 = p - a, v3 = p - b;
+    if (dcmp(dot(v1, v2)) < 0) return length(v2);
+    if (dcmp(dot(v1, v3)) > 0) return length(v3);
+    return fabs(cross(v1, v2)) / length(v1);
+}
+
+/// 点在直线上的投影
+Point lineProjection(Point p, Point a, Point b) {
+    Vector v = b - a;
+    return a + v * (dot(v, p - a) / dot(v, v));
+}
+
+/// 两线段相交（不含端点）
+bool segmentProperIntersection(Point a1, Point a2, Point b1, Point b2) {
+    // c1 == c2 == 0 表示量线段共线（可能有重合部分）
+    // c1，c2 不全为零时有可能某个端点在另一条线段上
+    double c1 = cross(a2 - a1, b1 - a1), c2 = cross(a2 - a1, b2 - a1);
+    double c3 = cross(b2 - b1, a1 - b1), c4 = cross(b2 - b1, a2 - b1);
+    return dcmp(c1) * dcmp(c2) < 0 && dcmp(c3) * dcmp(c4) < 0;
+}
+
+/// 判 p 是否在 a1a2 上（不含端点）
+bool onSegment(Point p, Point a1, Point a2) {
+    return dcmp(cross(a1 - p, a2 - p)) == 0 && dcmp(dot(a1 - p, a2 - p)) < 0;
+}
+
+/**
+ 多边形的表示形式，为一个首地址为 p，长度为 n 的数组。
+ */
+struct Polygon {
+    Point *p;
+    int n;
+};
+
+/// 求多边形面积
+double area(Point *p, int n) {
+    double area = 0;
+    for (int i = 1; i < n - 1; i++)
+        area += cross(p[i] - p[0], p[i + 1] - p[0]);
+    return fabs(area * 0.5);
 }
