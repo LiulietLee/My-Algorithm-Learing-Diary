acae2bbe666381980d29e673b4101aff481e1083
_liet <lee.liuliet@hotmail.com>
2018-12-21 17:43:05 +0800

    Update [1, n] 异或和.cpp

--- 数学相关/[1, n] 异或和.cpp
+++ 数学相关/[1, n] 异或和.cpp
@@ -6,3 +6,16 @@ int xorSum(int n) {
     default: return 0; break;
     }
 }
+
+int xorSumEven(int n) {
+    switch ((n / 2) % 4) {
+        case 0: return n; break;
+        case 1: return 2; break;
+        case 2: return n + 2; break;
+        default: return 0; break;
+    }
+}
+
+int xorSumOdd(int n) {
+    return xorSum(n) ^ xorSumEven(n - 1);
+}
